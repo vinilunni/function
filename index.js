1c4f6be2a02dff7c41ce02cfa8047a1efe7c79f5
VINIL VARGHESE <vinilunni@gmail.com>
2022-12-15 14:55:25 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+
+
+const function = async (req, res) => {
+
+  // health check
+  if (req.params["health"] === "health") {
+    res.write(JSON.stringify({success: true, msg: "Health check success"}))
+    res.end()
+  }
+
+  // Add your code here
+  res.write(JSON.stringify({success: true, msg: `Hello function`}))
+  res.end()
+  
+}
+
+export default function
